6c9c6c7df4d9126ea1fce1a194b9efb4b1358e30
DanForgedFrameworks <dmbbms@gmail.com>
2026-04-04 08:58:01 +0100

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -165,7 +165,7 @@
   shell.className = "pageshell";
   shell.setAttribute("data-page-key", activeKey);
 
-  shell.innerHTML = `
+    shell.innerHTML = `
     ${headerHtml}
     <main id="main" class="pagemain">
       <div class="pagelayout ${wantsSidebar ? "has-sidebar" : "no-sidebar"}">
@@ -176,9 +176,6 @@
       </div>
     </main>
     ${footerHtml}
-    <div class="express-interest-float">
-      <a href="${EXPRESS_INTEREST_LINK}" data-mailto-hint="Opens your email app to contact Tidy Butt.">Express interest</a>
-    </div>
   `;
 
   const target = shell.querySelector("#pagepanel-inner");
